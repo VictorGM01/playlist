cb9cae5d9a1c4d0019b8e14a75d25ccb226eefa5
VictorGM01 <victormarques8801@gmail.com>
2021-07-31 16:23:17 -0300

    # Código de uma Playlist de Filmes e Séries

---
+++ playlist/modelo_playlist.py
@@ -0,0 +1,58 @@
+class Programas:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes +=1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return f"{self.nome} - {self.ano} - {self.likes} Likes"
+
+class Filme(Programas):
+
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    def __str__(self):
+        return f"{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes"
+
+
+class Serie(Programas):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+    def __str__(self):
+        return f"{self.nome} - {self.ano} - {self.temporadas} Temporadas - {self.likes} Likes"
+
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __len__(self):
+        return len(self._programas)
+
